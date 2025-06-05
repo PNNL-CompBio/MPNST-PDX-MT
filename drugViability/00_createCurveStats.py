12af01ac9f8644c15bfd3ad44ca9ba37a0eee6ea
Belinda B. Garana <75867404+BelindaBGarana@users.noreply.github.com>
2025-06-05 15:01:38 -0700

    Update 00_createCurveStats.py
    Compile combo drug screens

--- drugViability/00_createCurveStats.py
+++ drugViability/00_createCurveStats.py
@@ -9,8 +9,11 @@ import subprocess
 syn = sc.login()
 ##this is a pain, need to move to file
 
-filelist = syn.tableQuery("select entityId,individualID,specimenID from syn65473033 where dataType='drugScreen' AND dataSubtype='processed' AND assay='cell viability assay'").asDataFrame()
+filelist = syn.tableQuery("select entityId,individualID,specimenID from syn65473033 where dataType='drug screen' AND dataSubtype='processed' AND assay='cell viability assay'").asDataFrame()
 # raw files are mistakenly marked as 'processed' so check 'dataSubtype' within file
+
+comboFiles = pd.read_csv(syn.get("syn66330284").path)
+comboFiles = comboFiles[comboFiles['dataSubtype']=="processed"]
 
 ###pull files
 
@@ -44,7 +47,21 @@ for index,row in filelist.iterrows():
           #print(tempSingle)
           singledose.append(tempSingle)
           #print(len(singledose))
-
+          
+comboMulti = []
+for index,row in comboFiles.iterrows():
+  #print(row['id'])
+  dfile = pd.read_csv(syn.get(row['entityId']).path)
+  if all(dfile['dataSubtype'] == 'processed'):
+      dfile = dfile.reset_index()
+      
+      # replace nan with drug 1 or 2 names as appropriate
+      drug1 = dfile['drugOneName'].dropna().unique()[0]
+      dfile['drugOneName'].fillna(drug1, inplace=True)
+      drug2 = dfile['drugTwoName'].dropna().unique()[0]
+      dfile['drugTwoName'].fillna(drug2, inplace=True)
+      if all(dfile['drugOneName'] == drug1) & all(dfile['drugTwoName'] == drug2):
+          comboMulti.append(dfile)
 
 ####first fit multidose curves...
 if len(multidose) > 0:
@@ -92,14 +109,58 @@ if len(singledose) > 0:
     stab = stab[curve_cols]
 else:
     stab = pd.DataFrame()
-
+    
+    
 newtab =pd.concat([otab,stab])
-
 ##rename file
 newtab.to_csv('viability_curves.tsv',sep='\t',index=False)
 
 ##now add in single-point drug measurements
 
 #store on synapse
+syn.store(sc.File('mpnst_drug_response.tsv',parentId='syn52369034'))
 syn.store(sc.File('viability_curves.tsv',parentId='syn52369034'))
+    
+####first fit multidose curves...
+if len(comboMulti) > 0:
+    print("compiling multi combos: ", len(comboMulti))
+    fulltab = pd.concat(comboMulti)
+    fulltab = fulltab[fulltab['drugThreeName'].isna()]
+    
+    # replace blanks with 0 values
+    fulltab['drugOneConcentration'].fillna(0, inplace=True)
+    fulltab['drugTwoConcentration'].fillna(0, inplace=True)
+    
+    #fulltab['DOSE']=fulltab.concentration#+0.0001 # check if should add 0.0001? perhaps cNF data had 0 values
+    # combine cell line and time into "sample" column
+    fulltab['sample'] = fulltab['sampleName'] + "_" + fulltab['timePoint'].astype(str) + fulltab['timePointUnit'] # if need replicate, then use specimenID
+    fulltab['drug1.units'] = fulltab['concentrationUnit']
+    fulltab['drug2.units'] = fulltab['concentrationUnit']
+    fulltab = fulltab.rename(columns={"percentViability": "effect",
+                            "drugOneName": "drug1", "drugTwoName": "drug2",
+                            "drugOneConcentration": "drug1.conc", "drugTwoConcentration": "drug2.conc"})
+    # there was a percentViability == "147.128*" - seems like it might be 147.1288 but double check
+    # if any(fulltab['effect'].str.contains("\*")):
+    #     print("replacing asterisk")
+    #     fulltab["effect"] = fulltab["effect"].replace(r'\*','', regex=True)
+    fulltab['effect'] = pd.to_numeric(fulltab['effect'])/100.00 # convert to fraction e.g., 0.9 instead of 90%
+    ##change file headers to DOSE/RESPONSE values needed by other script
+    fulltab['expt.date'] = "2025-06-05"
+    ncols=['expt.date','drug1.conc','drug2.conc','effect','sample','drug1','drug2','drug1.units','drug2.units']
+    fulltab2 = fulltab[ncols]
+    fulltab2.to_csv('mpnst_combo_drug_response.csv', index=False)
+    
+    # also create redacted version for upload to MuSyC web app
+    fulltab3 = fulltab
+    fulltab3['sampleName'] = fulltab3['sampleName'].str[-3:]
+    fulltab3['sample'] = fulltab3['sampleName'] + "_" + fulltab3['timePoint'].astype(str) + fulltab3['timePointUnit'] # if need replicate, then use specimenID
+    fulltab3['drug1'] = fulltab3['drug1'].str[1:4]
+    fulltab3['drug2'] = fulltab3['drug2'].str[1:4]
+    fulltab3 = fulltab3[ncols]
+    fulltab3.to_csv('combo_drug_response.csv', index=False) # MuSyC expects CSV file
+
+##now add in single-point drug measurements
+
+#store on synapse
+syn.store(sc.File('mpnst_combo_drug_response.csv',parentId='syn52369040'))
 
