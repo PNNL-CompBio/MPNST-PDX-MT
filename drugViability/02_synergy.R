f0664bd968de7bda9ce6e533f9758860b484f362
Belinda B. Garana <75867404+BelindaBGarana@users.noreply.github.com>
2025-09-11 14:32:43 -0700

    Update 02_synergy.R
    Add median and mean Bliss plots, update spider plots

--- drugViability/02_synergy.R
+++ drugViability/02_synergy.R
@@ -106,14 +106,17 @@ ggsave("musyc_meanLogAlpha_heatmap.pdf", width=5,height=4)
 
 bliss.musyc <- merge(bliss, musyc.scores, by=c("drugCombo","sample","time"))
 max.bliss <- plyr::ddply(bliss, .(drugCombo, sample, time), summarize,
-                         maxBliss = max(Bliss_synergy)) # 125
+                         maxBliss = max(Bliss_synergy),
+                         medianBliss = median(Bliss_synergy)) # 125
 write.csv(max.bliss,"maxBliss.csv", row.names=FALSE)
 
 mean.conf$conc1 <- mean.conf$drug1.conc
 mean.conf$conc2 <- mean.conf$drug2.conc
 bliss.tested <- merge(bliss, mean.conf, by=c("sample","time","drug1","drug2","conc1","conc2")) # 2529 down from 10287 bliss
 max.bliss.tested <- plyr::ddply(bliss.tested, .(drugCombo, sample, time), summarize,
-                                maxBliss = max(Bliss_synergy)) # 111
+                                maxBliss = max(Bliss_synergy),
+                                medianBliss = median(Bliss_synergy),
+                                meanBliss = mean(Bliss_synergy)) # 111
 write.csv(max.bliss.tested,"maxBlissTested.csv", row.names=FALSE)
 
 bliss.musyc <- merge(max.bliss, musyc.scores, by=c("drugCombo","sample","time"))
@@ -123,14 +126,29 @@ ggplot(bliss.musyc, aes(x=meanLogAlpha, y=maxBliss, shape=sample, color=drugComb
   labs(x="Mean MuSyC Potency", y="Max Bliss Synergy")
 ggsave("musyc_meanLogAlpha_vs_max_bliss_synergy.pdf", width=10, height=8)
 
+ggplot(bliss.musyc, aes(x=meanLogAlpha, y=medianBliss, shape=sample, color=drugCombo)) + #, size=R2)) +
+  geom_point()+theme_minimal() + 
+  #ggrepel::geom_text_repel(aes(label=paste0(time,"d"))) + 
+  labs(x="Mean MuSyC Potency", y="Max Bliss Synergy")
+ggsave("musyc_meanLogAlpha_vs_median_bliss_synergy.pdf", width=10, height=8)
+
 ggplot(bliss.musyc, aes(x=meanLogAlpha, y=maxBliss, shape=sample, color=drugCombo)) + #, size=R2)) +
   geom_point()+theme_minimal() + facet_grid(paste0(time,"d")~sample)+
   #ggrepel::geom_text_repel(aes(label=paste0(time,"d"))) + 
   labs(x="Mean MuSyC Potency", y="Max Bliss Synergy")
 ggsave("musyc_meanLogAlpha_vs_max_bliss_synergy_Faceted.pdf", width=12, height=7)
 
+ggplot(bliss.musyc, aes(x=meanLogAlpha, y=medianBliss, shape=sample, color=drugCombo)) + #, size=R2)) +
+  geom_point()+theme_minimal() + facet_grid(paste0(time,"d")~sample)+
+  #ggrepel::geom_text_repel(aes(label=paste0(time,"d"))) + 
+  labs(x="Mean MuSyC Potency", y="Max Bliss Synergy")
+ggsave("musyc_meanLogAlpha_vs_median_bliss_synergy_Faceted.pdf", width=12, height=7)
+
 cor.test(bliss.musyc$meanLogAlpha, bliss.musyc$maxBliss) # r = 0.011, p=0.89
 cor.test(bliss.musyc$meanLogAlpha, bliss.musyc$maxBliss, method="spearman") # rho = 0.039, p = 0.6151
+
+cor.test(bliss.musyc$meanLogAlpha, bliss.musyc$medianBliss) # r = 0.1604656, p=0.0389
+cor.test(bliss.musyc$meanLogAlpha, bliss.musyc$medianBliss, method="spearman") # rho = 0.109585, p = 0.1599
 
 bliss.musyc.tested <- merge(max.bliss.tested, musyc.scores, by=c("drugCombo","sample","time"))
 ggplot(bliss.musyc.tested, aes(x=meanLogAlpha, y=maxBliss, shape=sample, color=drugCombo)) + #, size=R2)) +
@@ -147,6 +165,21 @@ ggsave("musyc_meanLogAlpha_vs_max_bliss_synergy_tested_Faceted.pdf", width=12, h
 
 cor.test(bliss.musyc.tested$meanLogAlpha, bliss.musyc.tested$maxBliss) # r=0.066, p=0.3994
 cor.test(bliss.musyc.tested$meanLogAlpha, bliss.musyc.tested$maxBliss, method="spearman") # rho = 0.1481161, p=0.0561
+
+ggplot(bliss.musyc.tested, aes(x=meanLogAlpha, y=medianBliss, shape=sample, color=drugCombo)) + #, size=R2)) +
+  geom_point()+theme_minimal() + 
+  #ggrepel::geom_text_repel(aes(label=paste0(time,"d"))) + 
+  labs(x="Mean MuSyC Potency", y="Median Bliss Synergy")
+ggsave("musyc_meanLogAlpha_vs_median_bliss_synergy_tested.pdf", width=10, height=8)
+
+ggplot(bliss.musyc.tested, aes(x=meanLogAlpha, y=medianBliss, shape=sample, color=drugCombo)) + #, size=R2)) +
+  geom_point()+theme_minimal() + facet_grid(paste0(time,"d")~sample)+
+  #ggrepel::geom_text_repel(aes(label=paste0(time,"d"))) + 
+  labs(x="Mean MuSyC Potency", y="Median Bliss Synergy")
+ggsave("musyc_meanLogAlpha_vs_median_bliss_synergy_tested_Faceted.pdf", width=12, height=7)
+
+cor.test(bliss.musyc.tested$meanLogAlpha, bliss.musyc.tested$medianBliss) # r=0.066, p=0.3994
+cor.test(bliss.musyc.tested$meanLogAlpha, bliss.musyc.tested$medianBliss, method="spearman") # rho = 0.1481161, p=0.0561
 
 #### bliss heatmap ####
 maxAbsBliss <- max(abs(max.bliss.tested$maxBliss)) # 65.87242
@@ -177,6 +210,20 @@ ggplot(max.bliss[max.bliss$maxBliss != 0,], aes(x=sample, y=reorder(drugCombo, m
   theme(axis.title = element_blank(), axis.text.x = element_text(angle=45, vjust=1, hjust=1)) + labs(fill="Log|Max Bliss|")
 ggsave("bliss_maxSynergy_log10_heatmap.pdf", width=4.5,height=4)
 
+maxAbsBliss <- max(abs(max.bliss.tested$medianBliss)) # 0
+minMaxBliss <- min(max.bliss.tested$medianBliss) # 0
+ggplot(max.bliss.tested, aes(x=sample, y=reorder(drugCombo, medianBliss), fill=medianBliss)) + geom_tile(stat="identity") + 
+  facet_wrap(.~paste0(time,"d"))+theme_classic() + scale_fill_gradient(low="grey",high="red",limits=c(minMaxBliss,maxAbsBliss)) + 
+  theme(axis.title = element_blank(), axis.text.x = element_text(angle=45, vjust=1, hjust=1)) + labs(fill="Median Bliss")
+ggsave("bliss_medianSynergyTested_heatmap.pdf", width=4.5,height=4)
+
+maxAbsBliss <- max(abs(max.bliss.tested$meanBliss)) # 24.871
+minMaxBliss <- min(max.bliss.tested$meanBliss) # -24.871
+ggplot(max.bliss.tested, aes(x=sample, y=reorder(drugCombo, meanBliss), fill=meanBliss)) + geom_tile(stat="identity") + 
+  facet_wrap(.~paste0(time,"d"))+theme_classic() + scale_fill_gradient2(low="blue",mid="grey",high="red",limits=c(minMaxBliss,maxAbsBliss)) + 
+  theme(axis.title = element_blank(), axis.text.x = element_text(angle=45, vjust=1, hjust=1)) + labs(fill="Mean Bliss")
+ggsave("bliss_meanSynergyTested_heatmap.pdf", width=4.5,height=4)
+
 #### spider plots ####
 # 6 drug combos tested in all 6 MPNSTs at 2 timepoints: mean MuSyC LogAlpha, median CI, median Bliss
 library(fmsb)
@@ -189,12 +236,16 @@ colnames(ci)[1] <- "drugCombo"
 ci$drugCombo <- tolower(ci$drugCombo)
 ci <- reshape2::melt(ci, id.vars=c("drugCombo","time"), variable.name="sample", value.name="CI")
 ci$sample <- gsub("[.]","-",ci$sample)
-maxmin <- data.frame(minCI = floor(min(ci$CI, na.rm=TRUE)),maxCI = ceiling(max(ci$CI, na.rm=TRUE)),
+ci$rank <- -ci$CI # ci < 1 denotes synergy, =1: additive, <1: antagonistic
+ci$CI <- NULL
+maxmin <- data.frame(minCI = floor(min(ci$rank, na.rm=TRUE)),maxCI = ceiling(max(ci$rank, na.rm=TRUE)),
                      minBliss = floor(min(bliss.musyc.tested$maxBliss, na.rm=TRUE)), maxBliss = ceiling(max(bliss.musyc.tested$maxBliss, na.rm=TRUE)),
                      minMusyc = floor(min(bliss.musyc.tested$meanLogAlpha, na.rm=TRUE)), maxMusyc = ceiling(max(bliss.musyc.tested$meanLogAlpha, na.rm=TRUE)))
 maxmin2 <- data.frame("CI" = c(maxmin$maxCI, maxmin$minCI), "Bliss" = c(maxmin$maxBliss, maxmin$minBliss), "MuSyC" = c(maxmin$maxMusyc, maxmin$minMusyc))
 rownames(maxmin2) <- c("max","min")
-synergy <- merge(ci, bliss.musyc.tested[,c("drugCombo","sample","time","maxBliss","meanLogAlpha")], by=c("drugCombo","sample","time")) # 53 rows for 13 drugCombos
+bliss.musyc.tested$drugCombo <- tolower(bliss.musyc.tested$drugCombo)
+ci$drugCombo <- tolower(ci$drugCombo)
+synergy <- merge(na.omit(ci), bliss.musyc.tested[,c("drugCombo","sample","time","maxBliss","meanLogAlpha")], by=c("drugCombo","sample","time")) # 53 rows for 13 drugCombos
 # comboCoverage <- plyr::ddply(synergy, .(drugCombo), summarize,
 #                              nSamples=length(unique(sample))) # only mirda+palbo, mirda+vorin, mirda+trab have full coverage (6 samples); NAs at 5d CI in TNO combos
 # maxSamples <- length(unique(synergy$sample)) # 6
@@ -209,26 +260,26 @@ names(allSampleColors) <- unique(synergyFull$sample)
 
 for (t in unique(synergyFull$time)) {
   for (dc in unique(synergyFull$drugCombo)) {
-  # reformat data so that each column is an axis variable, rownames are samples except for first 2 rows which are max and min
-  tempSynergy <- synergyFull[synergyFull$time == t & synergyFull$drugCombo == dc,]
-  rownames(tempSynergy) <- tempSynergy$sample
-  data <- tempSynergy[,c("CI","maxBliss","meanLogAlpha")]
-  colnames(data) <- c("CI", "Bliss", "MuSyC")
-  #data <- rbind(maxmin2, tempSynergy)
-  sampleColors <- allSampleColors[rownames(data)]
-  
-  # prep to save plot
-  pdf(paste0(t,"d_",dc,"_spiderPlot.pdf"), width=5, height=4)
-  
-  # create plot
-  fmsb::radarchart(data, axistype=0, maxmin=FALSE, pcol=sampleColors, 
-                   pfcol=alpha(sampleColors,0.3), plwd=4, plty=1, cglcol="gray",
-                   cglty=1, axislabcol="black", cglwd=0.8, vlcex=0.8)
-  legend(x=0.75, y=1.25, legend=rownames(data), bty="n", pch=20, 
-         col=sampleColors, text.col="black", cex=0.8, pt.cex=1.2) 
-  
-  # save plot
-  dev.off()
+    # reformat data so that each column is an axis variable, rownames are samples except for first 2 rows which are max and min
+    tempSynergy <- synergyFull[synergyFull$time == t & synergyFull$drugCombo == dc,]
+    rownames(tempSynergy) <- tempSynergy$sample
+    data <- tempSynergy[,c("rank","maxBliss","meanLogAlpha")]
+    colnames(data) <- c("CI", "Bliss", "MuSyC")
+    #data <- rbind(maxmin2, tempSynergy)
+    sampleColors <- allSampleColors[rownames(data)]
+    if (length(unique(data$Bliss))>1 & length(unique(data$CI)) > 1 & length(unique(data$MuSyC)) > 1) {
+      # prep to save plot
+      pdf(paste0(t,"d_",dc,"_spiderPlot.pdf"), width=5, height=4)
+      
+      # create plot
+      fmsb::radarchart(data, axistype=0, maxmin=FALSE, pcol=sampleColors, 
+                       pfcol=alpha(sampleColors,0.3), plwd=4, plty=1, cglcol="gray",
+                       cglty=1, axislabcol="black", cglwd=0.8, vlcex=0.8)
+      legend(x=0.75, y=1.25, legend=rownames(data), bty="n", pch=20, 
+             col=sampleColors, text.col="black", cex=0.8, pt.cex=1.2) 
+      
+      # save plot
+      dev.off() 
+    }
   }
 }
-# was only enough data for plots of mirda+palbo, mirda+trab, mirda+vorin each at 2 or 5d
