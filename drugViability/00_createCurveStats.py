6c0e42b6105b86013faa476f2250f6a4accbc7bb
Sara JC Gosline <sara.gosline@pnnl.gov>
2025-09-17 14:27:48 -0700

    updated to group drugs by experiment

--- drugViability/00_createCurveStats.py
+++ drugViability/00_createCurveStats.py
@@ -13,13 +13,16 @@ syn = sc.login()
 
 #comboFilelist = syn.tableQuery("select entityId,individualID,specimenID from syn65473033 where dataType='drug screen' AND dataSubtype='processed' AND assay='cell viability assay'").asDataFrame()
 # raw files are mistakenly marked as 'processed' so check 'dataSubtype' within file
-comboFiles = pd.read_csv(syn.get("syn66330284").path) # previous: syn66330284 but points to combo
-comboFiles = comboFiles[comboFiles['dataSubtype']=="processed"]
-
-singleFiles = pd.read_csv(syn.get("syn65473034").path) # previous: syn66330284 but points to combo
-singleFiles = singleFiles[singleFiles['dataSubtype']=="processed"]
+#comboFiles = pd.read_csv(syn.get("syn66330284").path) # previous: syn66330284 but points to combo
+#comboFiles = comboFiles[comboFiles['dataSubtype']=="processed"]
+comboFiles = syn.tableQuery("SELECT * FROM syn52369043 where parentId='syn66330226'")
+comboFiles = comboFiles.asDataFrame()
+#singleFiles = pd.read_csv(syn.get("syn65473034").path) # previous: syn66330284 but points to combo
+#singleFiles = singleFiles[singleFiles['dataSubtype']=="processed"]
+singleFiles = syn.tableQuery("SELECT * FROM syn52369043 where parentId='syn65473019'")
+singleFiles = singleFiles.asDataFrame()
 # remove data from 2025-01-22 due to different method where media was refreshed
-singleFiles = singleFiles[~singleFiles.Filename.str.contains("250122.csv")]
+singleFiles = singleFiles[~singleFiles.name.str.contains("250122.csv")]
 
 ###pull files
 
@@ -28,7 +31,7 @@ multidose = []
 comboMulti = []
 for index,row in comboFiles.iterrows():
   #print(row['id'])
-  dfile = pd.read_csv(syn.get(row['entityId']).path)
+  dfile = pd.read_csv(syn.get(row['id']).path)
   if all(dfile['dataSubtype'] == 'processed'):
       dfile['improve_sample_id'] = row['specimenID']
       dfile = dfile.reset_index()
@@ -43,7 +46,7 @@ for index,row in comboFiles.iterrows():
               
 for index,row in singleFiles.iterrows():
   #print(row['id'])
-  dfile = pd.read_csv(syn.get(row['entityId']).path)
+  dfile = pd.read_csv(syn.get(row['id']).path)
   if all(dfile['dataSubtype'] == 'processed'):
       dfile['improve_sample_id'] = row['specimenID']
       dfile = dfile.reset_index()
@@ -71,7 +74,7 @@ for index,row in singleFiles.iterrows():
               singledose.append(tempSingle)
               #print(len(singledose))
 
-####first fit multidose curves...
+##first fit multidose curves...
 #os.chdir("/Users/gara093/Library/CloudStorage/OneDrive-PNNL/Documents/GitHub/MPNST-PDX-MT/drugViability/")
 if len(multidose) > 0:
     print("compiling multi: ", len(multidose))
@@ -86,7 +89,7 @@ if len(multidose) > 0:
     ncols=['DOSE','GROWTH','study','source','improve_sample_id','Drug','time','time_unit']
     fulltab = fulltab[ncols]
     # there was a percentViability == "147.128*" - seems like it might be 147.1288 but double check
-    # if any(fulltab['GROWTH'].str.contains("\*")):
+    #if any(fulltab['GROWTH'].str.contains("\*")):
     #     print("replacing asterisk")
     #     fulltab["GROWTH"] = fulltab["GROWTH"].replace(r'\*','', regex=True)
     fulltab['GROWTH'] = pd.to_numeric(fulltab['GROWTH'])
@@ -94,9 +97,9 @@ if len(multidose) > 0:
     fulltab.to_csv('mpnst_drug_response.tsv',sep='\t')
     
     ##fit curve
-    #if not os.path.exists("fit_curve.py"):
     script='https://raw.githubusercontent.com/PNNL-CompBio/coderdata/refs/heads/main/coderbuild/utils/fit_curve.py'
-    subprocess.run(['wget',script])
+    if not os.path.exists("fit_curve.py"):
+      subprocess.run(['wget',script])
     #subprocess.run(['python3','-m','pip','install','matplotlib']) # due to matplotlib error
     subprocess.run(['python3','fit_curve.py','--input','mpnst_drug_response.tsv','--output','mpnstDrugOutput'])
     #runpy.run_path("fit_curve.py") # replaces subprocess call
@@ -106,25 +109,26 @@ else:
     otab = pd.DataFrame()
 
 #####now we can take single drug points and format those
-if len(singledose) > 0:
-    print("compiling single: ", len(singledose))
-    stab = pd.concat(singledose)
-    stab = stab.rename(columns={"drugName": "improve_drug_id", 
-                            "timePoint": "time", "timePointUnit": "time_unit"})
-    stab['study']='mpnstPDXMT'
-    stab['source']='synapse'
-    stab['percentViability'] = pd.to_numeric(stab['percentViability'])
-    stab['dose_response_value'] = stab.percentViability/100.00
-    stab['dose_response_metric'] = 'uM_viability' # not sure, but concentrationUnit='uM' and assay='cell viability assay', platform='3D CellTiter-Glo'
+##there should be none for this dataset
+# if len(singledose) > 0:
+#     print("compiling single: ", len(singledose))
+#     stab = pd.concat(singledose)
+#     stab = stab.rename(columns={"drugName": "improve_drug_id", 
+#                             "timePoint": "time", "timePointUnit": "time_unit"})
+#     stab['study']='mpnstPDXMT'
+#     stab['source']='synapse'
+#     stab['percentViability'] = pd.to_numeric(stab['percentViability'])
+#     stab['dose_response_value'] = stab.percentViability#/100.00
+#     stab['dose_response_metric'] = 'uM_viability' # not sure, but concentrationUnit='uM' and assay='cell viability assay', platform='3D CellTiter-Glo'
 
-    curve_cols = ['source','improve_sample_id','improve_drug_id','study','time','time_unit',
-              'dose_response_metric','dose_response_value']
-    stab = stab[curve_cols]
-else:
-    stab = pd.DataFrame()
+#     curve_cols = ['source','improve_sample_id','improve_drug_id','study','time','time_unit',
+#               'dose_response_metric','dose_response_value']
+#     stab = stab[curve_cols]
+# else:
+#     stab = pd.DataFrame()
     
     
-newtab =pd.concat([otab,stab])
+newtab = otab #pd.concat([otab,stab])
 ##rename file
 newtab.to_csv('viability_curves.tsv',sep='\t',index=False)
 
@@ -134,7 +138,7 @@ newtab.to_csv('viability_curves.tsv',sep='\t',index=False)
 syn.store(sc.File('mpnst_drug_response.tsv',parentId='syn52369034'))
 syn.store(sc.File('viability_curves.tsv',parentId='syn52369034'))
     
-####first fit multidose curves...
+####now we move to the drug combinations
 if len(comboMulti) > 0:
     print("compiling multi combos: ", len(comboMulti))
     fulltab = pd.concat(comboMulti)
@@ -153,17 +157,21 @@ if len(comboMulti) > 0:
                             "drugOneName": "drug1", "drugTwoName": "drug2",
                             "drugOneConcentration": "drug1.conc", "drugTwoConcentration": "drug2.conc"})
     # there was a percentViability == "147.128*" - seems like it might be 147.1288 but double check
-    # if any(fulltab['effect'].str.contains("\*")):
+    #if any(fulltab['effect'].str.contains("\*")):
     #     print("replacing asterisk")
     #     fulltab["effect"] = fulltab["effect"].replace(r'\*','', regex=True)
-    fulltab['effect'] = pd.to_numeric(fulltab['effect'])/100.00 # convert to fraction e.g., 0.9 instead of 90%
+    #we need percentages (SG):
+    fulltab['effect'] = pd.to_numeric(fulltab['effect'])#/100.00 # convert to fraction e.g., 0.9 instead of 90%
     ##change file headers to DOSE/RESPONSE values needed by other script
     fulltab['expt.date'] = date.today()
     ncols=['expt.date','drug1.conc','drug2.conc','effect','sample','drug1','drug2','drug1.units','drug2.units']
     fulltab2 = fulltab[ncols]
     fulltab2.to_csv('mpnst_combo_drug_response.csv', index=False)
+
     
-    # also calculate auc values
+    # also calculate auc values for the combinations
+    fulltab['improve_sample_id'] = fulltab['improve_sample_id']+'_'+fulltab['drug1']+'+'+fulltab['drug2']
+        
     tabs1 = fulltab[(fulltab['drug1.conc']>0) & (fulltab['drug2.conc']==0)]
     tabs1 = tabs1.rename(columns={"drug1.conc": "DOSE", "effect": "GROWTH",
                             "timePoint": "time", "timePointUnit": "time_unit",
@@ -180,20 +188,25 @@ if len(comboMulti) > 0:
     tabsCombo2['Drug'] = tabsCombo2['drug1']+'+'+tabsCombo2['drug2']#+'('+round(tabsCombo2['drug1.conc']/tabsCombo2['drug2.conc'],2).astype(str)+')2'
     tabsCombo2 = tabsCombo2.rename(columns={"drug2.conc": "DOSE", "effect": "GROWTH",
                             "timePoint": "time", "timePointUnit": "time_unit"})
+
+
+    
     ncols0=['DOSE','GROWTH','improve_sample_id','Drug','time','time_unit']
     fulltab = pd.concat([tabs1[ncols0], tabs2[ncols0], tabsCombo1[ncols0], tabsCombo2[ncols0]], ignore_index=True)
+
+
     #fulltab = pd.concat([tabs1, tabs2, tabsCombo1, tabsCombo2])
     fulltab['study']='mpnstPDXMT'
     fulltab['source']='synapse'
     ##mutate the values create new columns
     ncols=['DOSE','GROWTH','study','source','improve_sample_id','Drug','time','time_unit']
     fulltab = fulltab[ncols]
-    # there was a percentViability == "147.128*" - seems like it might be 147.1288 but double check
-    # if any(fulltab['GROWTH'].str.contains("\*")):
+    #there was a percentViability == "147.128*" - seems like it might be 147.1288 but double check
+    #if any(fulltab['GROWTH'].str.contains("\*")):
     #     print("replacing asterisk")
     #     fulltab["GROWTH"] = fulltab["GROWTH"].replace(r'\*','', regex=True)
     #SG: script requires percentages, so converting growth values to percentages here
-    fulltab['GROWTH'] = pd.to_numeric(fulltab['GROWTH'])*100.00
+    fulltab['GROWTH'] = pd.to_numeric(fulltab['GROWTH'])#*100.00
     ##change file headers to DOSE/RESPONSE values needed by other script
     fulltab.to_csv('mpnst_combo_drug_response_forCurves.tsv',sep='\t')
     
@@ -238,6 +251,8 @@ if len(comboMulti) > 0:
 
     combo = combo.merge(s1)
     combo = combo.merge(s2)
+    combo[['sample','experiment']] = combo['improve_sample_id'].str.split('_',expand=True)
+    combo = combo[['sample','experiment','drug1','drug2','time','time_unit','aac','auc','dss','fit_auc','fit_ec50','fit_ec50se','fit_einf','fit_hs','fit_r2','drug1_auc','drug2_auc']]
     combo.to_csv('mpnstDrugComboMatrix.tsv',sep='\t')
 ##now add in single-point drug measurements
 
